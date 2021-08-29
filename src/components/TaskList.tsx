9a155d6eed5896353dd6d3a88261c207b074789a
Ian Lucas Paim <34959723+oianlucas@users.noreply.github.com>
2021-08-29 20:00:17 -0300

    Update TaskList. All tests passed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,29 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+    const addTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+    setTasks( oldState => [...oldState, addTask] );
+    setNewTaskTitle(''); 
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const completedTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete,
+    } : task )
+    setTasks(completedTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id );
+    setTasks(filteredTasks);
   }
 
   return (
